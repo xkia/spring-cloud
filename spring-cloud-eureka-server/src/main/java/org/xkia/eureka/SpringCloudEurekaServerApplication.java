00ec46608ad666d6f22cb89217e8f267957a1ed6
xkia <xukai8023@gmail.com>
2018-06-15 16:33:23 +0800

    init 1

---
+++ spring-cloud-eureka-server/src/main/java/org/xkia/eureka/SpringCloudEurekaServerApplication.java
@@ -0,0 +1,17 @@
+package org.xkia.eureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * @author xkia
+ */
+@EnableEurekaServer
+@SpringBootApplication
+public class SpringCloudEurekaServerApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(SpringCloudEurekaServerApplication.class, args);
+  }
+}
--- spring-cloud-producer/src/main/java/org/xkia/SpringCloudProducerApplication.java
+++ spring-cloud-producer/src/main/java/org/xkia/SpringCloudProducerApplication.java
@@ -1,18 +1,14 @@
 package org.xkia;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
 
 /**
  * @author xkia
  */
-@Configuration
-@ComponentScan
-@EnableAutoConfiguration
 @EnableEurekaClient
+@SpringBootApplication
 public class SpringCloudProducerApplication {
 
   public static void main(String[] args) {
